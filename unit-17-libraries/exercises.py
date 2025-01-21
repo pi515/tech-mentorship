b154e973b93c7ae8e85fd97bf4af4cfe9bae3399
jayeon <12667837+jayeon@users.noreply.github.com>
2025-01-21 09:43:32 -0600

    add exercises

---
+++ unit-17-libraries/exercises.py
@@ -0,0 +1,15 @@
+import random
+# Exercise 1: Import the math library, using Line 1 as a reference
+# Your code here:
+
+# Exercise 2: 
+# You saw the `random.randint()` method in an earlier example
+# How would you use the method to simulate a six-sided dice roll?
+# Your code here:
+
+# Exercise 3: 
+# Now it's time to read the documentation for random:
+# https://docs.python.org/3/library/random.html#functions-for-sequences
+# What method in random would you use to simulate a game of rock, paper, scissors?
+# Hint: No need to scroll too far
+# Your code here:
